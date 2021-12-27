c76e3957296de83f2ff1352b7211a20c02da0fde
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-27 16:08:56 +0100

    Unit test with property injection instead of constructor injection

---
+++ net60/module5/before/PersonDataReader.CSV.Tests/FakeFileLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace PersonDataReader.CSV.Test;
+
+public class FakeFileLoader : ICSVFileLoader
+{
+    private string _dataType;
+
+    public FakeFileLoader(string dataType)
+    {
+        _dataType = dataType;
+    }
+
+    public IReadOnlyCollection<string> LoadFile()
+    {
+        return _dataType switch
+        {
+            "Good" => TestData.WithGoodRecords,
+            "Mixed" => TestData.WithGoodAndBadRecords,
+            "Bad" => TestData.WithOnlyBadRecords,
+            "Empty" => new List<string>(),
+            _ => TestData.WithGoodAndBadRecords,
+        };
+    }
+
+    IReadOnlyCollection<string> ICSVFileLoader.LoadFile()
+    {
+        return this.LoadFile();
+    }
+}
